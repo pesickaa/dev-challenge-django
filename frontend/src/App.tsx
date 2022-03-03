55451e06a081f731989336da93a8ad50d04ef1e2
Alexis <alexispesicka@gmail.com>
2022-03-03 15:52:39 +0000

    Branch/pesicka solution (#1)
    * Adding in initial commits, dealing with cors policy issue
    
    * Moving calculations to appropriate endpoint and only passing back variables needed for Graph.
    
    * More styling for inputs using Chakra-ui
    
    * Fixing the input for interest rate from adding extra decimals.
    
    Co-authored-by: Alexis Pesicka <alexis@simplyvideo.io>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,105 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
 import DefaultLayout from './components/layouts/Default'
 import LineChart from './components/LineChart'
 import theme from './theme'
+import axios from 'axios'
+import { Input } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+  } from '@chakra-ui/react'
 
 const defaultTheme = extendTheme(theme)
-
-// Note: This is just for example purposes
-// should be replaced with real data from the server
-const tempData = {
-    xAxis: [0, 1, 2, 3, 4, 5],
-    yAxis: [100, 150, 180, 210, 240, 350],
-}
+const baseURL = "http://localhost:8000"
+const installments = 50;
 
 function App() {
-    return (
-        <ChakraProvider theme={defaultTheme}>
-            {/* We've just bundled everything into one file here to 
-            get you started!*/}
-            <DefaultLayout>
-                <Container pt={6}>
-                    <LineChart
-                        title="Savings Over time"
-                        xAxisData={tempData.xAxis}
-                        yAxisData={tempData.yAxis}
-                        xLabel="Years"
-                        yLabel="Amount"
-                    />
-                </Container>
-            </DefaultLayout>
-        </ChakraProvider>
-    )
+  const [principalDeposit, setPrincipalDeposit] = useState(0 as number);
+  const [monthlyDeposit, setMonthlyDeposit] = useState(100.00 as number);
+  const [interestRate, setInterestRate] = useState(0 as number);
+  
+  const [interestInstallments, setInterestInstallments] = useState([]);
+
+  const graphData = {
+    xAxis: Array.from({length: installments+1}, (v,i) => i),
+    yAxis: interestInstallments,
+  }
+
+  const handlePrincipalDeposit = ({ target }: { target: any }) => setPrincipalDeposit(target.value);
+  const handleMonthlyDeposit = ({ target }: { target: any }) => setMonthlyDeposit(target.value);
+  const handleInterestRate = ({ target }: { target: any }) => {
+      var value = target.value;
+      value = (value/100).toFixed(2);
+      setInterestRate(value as number);
+  };
+  
+  useEffect(() => {
+    if (principalDeposit !== 0 && interestRate !== 0) {
+        const data = {
+            interestRate: interestRate,
+            principalDeposit: principalDeposit,
+            monthlyDeposit: monthlyDeposit,
+            installments: installments,
+        }
+    
+        axios({url: "/interest-data/", baseURL: baseURL, data: data, method: 'POST' })
+            .then( (resp) => {
+                const interestData = resp.data['interest_over_installment'];
+                setInterestInstallments(interestData);
+            });
+    }
+    
+  }, [principalDeposit, monthlyDeposit, interestRate] );
+
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <DefaultLayout>
+        <Container pt={6}>
+          <LineChart
+            title="Savings Over time"
+            xAxisData={graphData.xAxis}
+            yAxisData={interestInstallments}
+            xLabel="Years"
+            yLabel="Amount"
+          />
+        </Container>
+        <Container p={4}>
+            <FormControl maxW={60}>
+                <FormLabel as='legend'>Initial Deposit: {principalDeposit}</FormLabel>
+                <Input
+                className="input"
+                value={principalDeposit}
+                name="principalDeposit"
+                onChange={handlePrincipalDeposit}
+                />
+            </FormControl>
+            <br/>
+            <FormControl maxW={60}>
+                <FormLabel as='legend'>Monthly Deposit: {monthlyDeposit}</FormLabel>
+                <Input
+                    className="input"
+                    value={monthlyDeposit}
+                    name="monthlyDeposit"
+                    onChange={handleMonthlyDeposit}
+                />
+            </FormControl>
+            <br/>
+            <FormControl maxW={60}>
+                <FormLabel as='legend'>Interest Rate: {interestRate} ({(interestRate*100).toFixed(2)}%)</FormLabel>
+                <Input
+                    className="input"
+                    value={Math.round(interestRate*100)}
+                    name="interestRate"
+                    onChange={handleInterestRate}
+                />
+            </FormControl>
+        </Container>
+      </DefaultLayout>
+    </ChakraProvider>
+  )
 }
 
 export default App
